dc29f3d79052fda0e5bd083799d58962128f0ed1
tonan <126637328+tonantr@users.noreply.github.com>
2023-09-14 23:23:19 -0400

    edit the database module

--- database.py
+++ database.py
@@ -2,14 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 import configparser
-import os
 
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-alembic_ini_path = os.path.join(script_dir, 'alembic.ini')
 
 config = configparser.ConfigParser()
-config.read(alembic_ini_path)
+config.read('alembic.ini')
 DATABASE_URL = config['alembic']['sqlalchemy.url']
 
 engine = create_engine(DATABASE_URL)
